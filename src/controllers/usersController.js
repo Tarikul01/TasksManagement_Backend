7696deef52ef9a5f8e954b1fe8d4fa296f02af74
Tarikul Islam <50962165+Tarikul01@users.noreply.github.com>
2023-02-19 22:11:53 +0600

    user controller changes

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -6,7 +6,7 @@ exports.registration=(req,res)=>{
     let reqBody=req.body;
     UserModel.create(reqBody,(err,data)=>{
         if(err){
-            res.status(400).json({status:"fail",data:err});
+            res.status(200).json({status:"fail",data:err});
 
         }else{
             res.status(200).json({status:"success",data:data})
